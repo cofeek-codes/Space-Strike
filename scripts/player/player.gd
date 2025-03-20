9a72a7666583a6bf4c870071130d83d22a739644
Cofeek <94117043+cofeek-codes@users.noreply.github.com>
2025-03-21 02:48:14 +0500

    shooting

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,26 +1,41 @@
 extends CharacterBody2D
 
-# Movement speed constants
-var horizontal_speed = 500.0
-var vertical_speed = 300.0
 
-@onready var animationPlayer: AnimatedSprite2D = $AnimationPlayer
- 
+const HORIZONTAL_SPEED = 500.0
+const VERTICAL_SPEED = 300.0
+
+var shooting_cooldown_timer = 0
+
 @onready var init_position = position
+
+@onready var aimMarker: Marker2D = $AimMarker
+
+var bullet_scene = preload("res://scenes/player/player_bullet.tscn")
 
 func handle_borders():
 	var viewport_dimensions = get_viewport_rect().size
 	if position.x > viewport_dimensions.x or position.x < 0 or position.y < 0 or position.y > viewport_dimensions.y:
 		position = init_position 
 
+func shooting(delta: float):
+	
+	shooting_cooldown_timer += delta
+	
+	if Input.is_action_just_pressed("shoot"):
+		print('shooted a bullet')
+		
+		var bullet = bullet_scene.instantiate()
+		get_parent().add_child(bullet)
+		bullet.global_position = aimMarker.global_position
+
 func movement():
 		# horizontal movement
 	var horizontal_input = Input.get_axis("move_left", "move_right")
-	velocity.x = horizontal_input * horizontal_speed
+	velocity.x = horizontal_input * HORIZONTAL_SPEED
 	
 	# vertical movement
 	var vertical_input = Input.get_axis("move_up", "move_down")
-	velocity.y = vertical_input * vertical_speed
+	velocity.y = vertical_input * VERTICAL_SPEED
 	
 	move_and_slide()
 
@@ -36,3 +51,4 @@ func _process(delta: float) -> void:
 func _physics_process(delta: float) -> void:
 	movement()
 	handle_borders()
+	shooting(delta)
