65be4b79fa97e3bcc2a7c7d3eeb282c79697edc7
Cofeek <94117043+cofeek-codes@users.noreply.github.com>
2025-03-29 22:57:30 +0500

    play button animation

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -3,6 +3,7 @@ extends Control
 @onready var title_label: RichTextLabel = $TitleLabel
 @onready var audio_player: AudioStreamPlayer = $AudioPlayer
 @onready var subtitle_label: RichTextLabel = $SubTitleLabel
+@onready var play_button: TextureButton = $PlayButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -25,3 +26,8 @@ func _on_title_animation_player_animation_finished(anim_name: StringName) -> voi
 		audio_player.play()
 		var subtitle_animation_player: AnimationPlayer = subtitle_label.get_child(0)
 		subtitle_animation_player.play('appear')
+		var play_button_animation_player: AnimationPlayer = play_button.get_child(0).get_child(0)
+		await get_tree().create_timer(0.5).timeout
+		play_button_animation_player.play('appear')
+		await get_tree().create_timer(1).timeout
+		play_button_animation_player.play('pulse')
