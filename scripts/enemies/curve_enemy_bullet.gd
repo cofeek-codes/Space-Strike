e0c8c7207f29812b20e07f703fa8e2e148982b11
Cofeek <94117043+cofeek-codes@users.noreply.github.com>
2025-03-31 16:17:04 +0500

    curve enemy bullet trail

--- scripts/enemies/curve_enemy_bullet.gd
+++ scripts/enemies/curve_enemy_bullet.gd
@@ -9,20 +9,19 @@ const LIFETIME = 5
 
 var time: float = 0.0
 
+var trail_point_queue: Array
+var TRAIL_POINT_QUEUE_MAX_LENGTH = 15
+
+
 @onready var timer: Timer = $DispawnTimer
 @onready var audio_player: AudioStreamPlayer = $AudioPlayer
+@onready var trail: Line2D = $Trail
 
 @onready var player: CharacterBody2D = get_node('../Player')
 
 
-
-func _ready() -> void:
-	audio_player.pitch_scale = randf_range(0.5, 1.5)
-	audio_player.play()
-
-func _physics_process(delta: float) -> void:
-	
-	# Update time for oscillation
+func movement(delta: float):
+		# Update time for oscillation
 	time += delta * OSCILLATION_FREQUENCY
 
 	# Calculate vertical offset using sine wave
@@ -34,6 +33,28 @@ func _physics_process(delta: float) -> void:
 	
 	# Apply movement
 	position -= movement
+
+
+func trailing():
+	
+	var pos = self.position
+	trail_point_queue.push_front(pos)
+	
+	if trail_point_queue.size() > TRAIL_POINT_QUEUE_MAX_LENGTH:
+		trail_point_queue.pop_back()
+		
+	trail.clear_points()
+	for point in trail_point_queue:	
+		trail.add_point(point)
+	
+
+func _ready() -> void:
+	audio_player.pitch_scale = randf_range(0.5, 1.5)
+	audio_player.play()
+
+func _physics_process(delta: float) -> void:
+	movement(delta)
+	trailing()
 
 
 func _on_dispawn_timer_timeout() -> void:
