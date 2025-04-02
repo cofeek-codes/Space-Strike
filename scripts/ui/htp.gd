43fa9fcfd5be6b98e2710044002ffdc25776ef15
Cofeek <94117043+cofeek-codes@users.noreply.github.com>
2025-04-03 03:12:33 +0500

    htp scroll fixed

--- scripts/ui/htp.gd
+++ scripts/ui/htp.gd
@@ -2,6 +2,10 @@ extends Control
 
 @onready var camera: Camera2D = $"../Camera"
 
+@onready var htp_label: RichTextLabel = $HowToPlayLabel
+
+func _ready() -> void:
+	htp_label.text = tr("HTP_TEXT") + "\n".repeat(5)
 
 func _on_back_button_pressed() -> void:
 	camera.position.y -= get_viewport_rect().size.y
