dfa4bb0fb0d227c5665d8e90311a0a8f4d05d18b
Cofeek <94117043+cofeek-codes@users.noreply.github.com>
2025-04-07 23:10:13 +0500

    htp localization and scroll fixed

--- scripts/ui/htp.gd
+++ scripts/ui/htp.gd
@@ -5,7 +5,7 @@ extends Control
 @onready var htp_label: RichTextLabel = $HowToPlayLabel
 
 func _ready() -> void:
-	htp_label.text = tr("HTP_TEXT") + "\n".repeat(5)
+	pass
 
 func _on_back_button_pressed() -> void:
 	camera.position.y -= get_viewport_rect().size.y
