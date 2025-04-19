e85492fa7cad0c904c1ce2a5b19e4872388a4175
Cofeek <94117043+cofeek-codes@users.noreply.github.com>
2025-04-20 00:21:32 +0500

    character select scene progress

---
+++ scripts/ui/character_select.gd
@@ -0,0 +1,19 @@
+extends Control
+
+@onready var current_skin: TextureRect = %CurrentSkin
+
+
+
+
+
+func _on_confirm_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+
+func _on_prev_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_next_button_pressed() -> void:
+	pass # Replace with function body.
